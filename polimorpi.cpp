c663172ec35deb9e4f30c5010a4836ab8eb9ebfc
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-06-02 14:21:42 +0700

    menambahkan class seseorang

---
+++ polimorpi.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class seseorang{
+public:
+    //Virtual void pesan() = 0;
+    virtual void pesan(){
+        cout <<"Pesan dari seseorang" <<endl;
+
+    }
+};
