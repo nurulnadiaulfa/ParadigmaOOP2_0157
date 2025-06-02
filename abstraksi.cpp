2d0b4c61786f464d0fca16d82da630fcc52a4837
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-06-02 14:51:19 +0700

    menambhkan akses modifier dan setXY di class abstraksiklas

---
+++ abstraksi.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class Abstraksiklas {
+private: string x, y;
+
+public:
+
+    //method untuk mengisi nilai
+    //private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
